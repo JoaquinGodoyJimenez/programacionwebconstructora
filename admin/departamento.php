88602fbcda0a7a30742f2ea29c9e5ddfb8c4fabe
JOAQUIN GODOY JIMENEZ <l19030394@celaya.tecnm.mx>
2023-03-09 14:24:07 -0600

    informacion faltante

--- admin/departamento.php
+++ admin/departamento.php
@@ -3,5 +3,12 @@ require_once('controllers/departamento.php');
 include_once('views/header.php');
 include_once('views/menu.php');
 include_once('views/footer.php');
-$data = $web -> getAll();
+$action = (isset($_GET['action']))?isset($_GET['action']):'getAll';
+switch($action){
+    case 'getAll':
+    default:
+        $data = $web->getAll();
+        include("views/departamento/index.php");
+}
+
 ?>
